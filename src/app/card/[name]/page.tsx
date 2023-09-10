5cc7075a659251291245bab75d8235765f7ef213
AndersonKeller <andersonkellervendas@hotmail.com>
2023-09-10 03:09:35 -0300

    feat: includes layout for Cardpage image

--- src/app/card/[name]/page.tsx
+++ src/app/card/[name]/page.tsx
@@ -1,12 +1,24 @@
+import { Cards } from "@/components/list/interfaces";
 import { api } from "@/service/api";
-
+import Image from "next/image";
+import style from "./cardPage.module.css";
 interface CardPageProps {
   params: { name: string };
 }
 
 export default async function CardPage({ params }: CardPageProps) {
   const res = await api.get(`?name=${params.name}`);
-  const card = res.data.data[0];
+  const card: Cards = res.data.data[0];
   console.log(card);
-  return <h1>{card.name} </h1>;
+  return (
+    <main className={style.mainContainer}>
+      <Image
+        className={style.cardImage}
+        alt={card.name}
+        src={card.card_images[0].image_url}
+        width={400}
+        height={700}
+      />
+    </main>
+  );
 }
