5c9040d8cda68526c084bfe6191e60dad6c70927
Anderson Keller <andersonkellervendas@hotmail.com>
2023-10-12 23:31:43 -0300

    feat: includes section styles and position to more infos nav

--- src/components/infos/Infosnav.tsx
+++ src/components/infos/Infosnav.tsx
@@ -1,7 +1,14 @@
+import { GrCircleInformation } from "react-icons/gr";
+import { ImPriceTags } from "react-icons/im";
 import { BackButton } from "./back/backButton";
+import styles from "./styles.module.css";
 export function InfosNav() {
   return (
-    <section>
+    <section className={styles.section}>
+      <div>
+        <GrCircleInformation className={styles.path} />
+        <ImPriceTags />
+      </div>
       <BackButton />
     </section>
   );
